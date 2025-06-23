444305840b3e1572d450d9e1d7149b82bdf1ec35
001TONYSTARK <97785214+001TONYSTARK@users.noreply.github.com>
2025-06-23 23:03:52 +0530

    Add files via upload
    Initial commit

---
+++ TaskManagerSpring/src/test/java/com/example/TaskManagerSpring/TaskManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskManagerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
